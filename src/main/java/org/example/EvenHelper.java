dcfb1c60f09ff47e08fabf425f8e02f36f1f3afa
louisdurampart <louis.durampart@gmail.com>
2023-06-27 10:31:42 +0200

    création Even Helper

---
+++ src/main/java/org/example/EvenHelper.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class EvenHelper {
+
+    public boolean isEven(int i) {
+        return (i % 2 == 0);
+    }
+
+}
--- src/test/java/org/example/EventHelperTest.java
+++ src/test/java/org/example/EventHelperTest.java
@@ -1,2 +1,20 @@
-package org.example;public class EventHelperTest {
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenHelperTest {
+
+    @Test
+    void checkIsEven() {
+        int[] numbers = {1, 2, 3, 4, 5, 23, 70, 89, 240};
+        EvenHelper evenHelper = new EvenHelper();
+
+        for (int n : numbers) {
+            boolean expectedResult = n % 2 == 1;
+            assertEquals(expectedResult, evenHelper.isEven(n));
+        }
+    }
 }
